f9de468d71cbecf3de0ca42fc41fdfbad4e8d9ba
urilixo <falecomelvio@hotmail.com>
2022-02-24 07:10:06 -0300

    Revert "Added models and route"
    This reverts commit a312bcc44a7b553a0a69814aeebebd918700ed17.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resource :users, only: [ :new, :create ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
